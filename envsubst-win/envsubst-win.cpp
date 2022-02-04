37d38249f1651d700ec905e4634e0c9fcc148dea
Luca Di Leo <ldileo@digipa.it>
2022-02-04 12:08:34 +0100

    Cleanup + remove existing output file

--- envsubst-win/envsubst-win.cpp
+++ envsubst-win/envsubst-win.cpp
@@ -34,6 +34,8 @@ int main(int argc, char* argv[], char* envp[])
 		return EXIT_FAILURE;
 	}
 
+	if (exists(dest)) fs::remove(dest);
+
 	std::ifstream input(source);
 	std::string content((std::istreambuf_iterator<char>(input)),(std::istreambuf_iterator<char>()));
 	input.close();
@@ -64,15 +66,3 @@ int main(int argc, char* argv[], char* envp[])
 	return EXIT_SUCCESS;
 
 }
-
-
-// Per eseguire il programma: CTRL+F5 oppure Debug > Avvia senza eseguire debug
-// Per eseguire il debug del programma: F5 oppure Debug > Avvia debug
-
-// Suggerimenti per iniziare: 
-//   1. Usare la finestra Esplora soluzioni per aggiungere/gestire i file
-//   2. Usare la finestra Team Explorer per connettersi al controllo del codice sorgente
-//   3. Usare la finestra di output per visualizzare l'output di compilazione e altri messaggi
-//   4. Usare la finestra Elenco errori per visualizzare gli errori
-//   5. Passare a Progetto > Aggiungi nuovo elemento per creare nuovi file di codice oppure a Progetto > Aggiungi elemento esistente per aggiungere file di codice esistenti al progetto
-//   6. Per aprire di nuovo questo progetto in futuro, passare a File > Apri > Progetto e selezionare il file con estensione sln
